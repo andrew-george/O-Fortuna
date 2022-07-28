0bac42bbb551e3c8cd0ab38edbb96a04b3cb176d
Andrew George <andrewgeorgeberty@gmail.com>
2022-07-28 23:28:00 +0200

    fully functional initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
+  //* states
+  const [lyrics, setLyrics] = useState(
+    'O Fortuna Velut luna Statu variabilis Semper crescis Aut decrescis Vita detestabilis Nunc obdurat Et tunc curat Ludo mentis aciem Egestatem Potestatem Dissolvit ut glaciem. Sors immanis Et inanis Rota tu volubilis Status malus Vana salus Semper dissolubilis Obumbrata Et velata Michi quoque niteris Nunc per ludum Dorsum nudum Fero tui sceleris Sors salutis Et virtutis Michi nunc contraria est affectus et defectus semper in angaria. Hac in hora Sine mora Corde pulsum tangite Quod per sortem Sternit fortem Mecum omnes plangite!\n'
+  );
+  const [output, setOutput] = useState('');
+  const [currentSplitParam, setCurrentSplitParam] = useState('Paragraphs');
+  const [numberInput, setNumberInput] = useState(1);
+
+  //* logic
+  function selectSplit(event) {
+    //-set split parameter
+    setCurrentSplitParam(event.target.innerText);
+
+    //- get siblings
+    const siblings = Array.from(event.target.parentElement.children);
+
+    //- loop over buttons remove selection
+    siblings.forEach(btn => (btn.style.backgroundColor = '#000'));
+
+    //- add selection to the clicked button
+    event.target.style.backgroundColor = '#111';
+  }
+  useEffect(
+    function generateText() {
+      if (numberInput < 0) return;
+      if (currentSplitParam === 'Characters') {
+        setOutput(lyrics.split('').slice(0, numberInput).join(''));
+      }
+      if (currentSplitParam === 'Words') {
+        setOutput(lyrics.split(' ').slice(0, numberInput).join(' '));
+      }
+      if (currentSplitParam === 'Paragraphs') {
+        setOutput(lyrics.repeat(numberInput));
+      }
+    },
+    [numberInput, currentSplitParam]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className='App'>
+      <p className='logo'>O</p>
+      <div className='container'>
+        <div className='title'>O Fortuna</div>
+        <div className='description'>
+          PLACEHOLDER TEXT GENERATOR WITH A LATIN TASTE
+        </div>
+        <div className='generator-container'>
+          <div className='control-panel'>
+            <div className='buttons-container'>
+              <button onClick={selectSplit}>Characters</button>
+              <button onClick={selectSplit}>Words</button>
+              <button onClick={selectSplit}>Paragraphs</button>
+            </div>
+            <input
+              type='number'
+              value={numberInput}
+              min='0'
+              onInput={event => {
+                setNumberInput(event.target.value);
+              }}
+            />
+          </div>
+          <textarea
+            id='output'
+            placeholder='Your text will appear here...'
+            value={output}
+            readOnly
+          ></textarea>
+        </div>
+        <a href='https://github.com/andrew-george' className='copyright'>
+          &copy; Designed & Developed by <span>Andrew Berty</span>
         </a>
-      </header>
+      </div>
     </div>
   );
 }
-
-export default App;
